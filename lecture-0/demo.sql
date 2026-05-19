1e8723eca44a5fb302fd6b1ce7b358374470a0ff
Mayuri Tapase <mayutapase@gmail.com>
2026-05-20 00:21:39 +0530

    Add like, OR,AND, BETWEEN queries.

--- lecture-0/demo.sql
+++ lecture-0/demo.sql
@@ -1,2 +1,23 @@
 select * from "longlist" limit 3;
+
 select "author" from "longlist";
+
+select "title", "author" from "longlist" where "year"=2022; 
+
+select "title", "author" from "longlist" where "year"=2021; 
+
+SELECT "title", "format" 
+FROM "longlist" 
+WHERE "format" != 'hardcover';
+
+SELECT "title", "format" 
+FROM "longlist" 
+WHERE "format" <> 'hardcover';
+
+SELECT "title", "format" 
+FROM "longlist" 
+WHERE NOT "format" = 'hardcover';
+
+SELECT "title", "author" 
+FROM "longlist" 
+WHERE "year" = 2022 OR "year" = 2023;
