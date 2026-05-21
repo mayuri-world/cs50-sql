243ccb720f2b5eb7ca033a20e40d733646a21c66
Mayuri Tapase <mayutapase@gmail.com>
2026-05-21 13:00:14 +0530

    Add null query

--- lecture-0/demo.sql
+++ lecture-0/demo.sql
@@ -21,3 +21,13 @@ WHERE NOT "format" = 'hardcover';
 SELECT "title", "author" 
 FROM "longlist" 
 WHERE "year" = 2022 OR "year" = 2023;
+
+select * FROM "longlist";
+select "title" FROM "longlist";
+select "title", "author" FROM longlist;
+SELECT "title" from "longlist" LIMIT 10;
+SELECT "title" , "author" FROM "longlist" WHERE "fromat" !='hardcover';
+SELECT "title", "author" FROM "longlist" WHERE "year"= 2023;
+SELECT "title", "format" FROM "longlist" WHERE "format" !='hardcover';
+SELECT "title", "translator" FROM "longlist" WHERE "translator" IS NULL;
+SELECT "title", "translator" FROM "longlist" WHERE "translator" IS NOT NULL;
