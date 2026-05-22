c4802b0f99d5fb8e4dce7525b6d7bf82895e2351
Mayuri Tapase <mayutapase@gmail.com>
2026-05-22 19:02:53 +0530

    Add Range queries

--- lecture-0/demo.sql
+++ lecture-0/demo.sql
@@ -31,3 +31,19 @@ SELECT "title", "author" FROM "longlist" WHERE "year"= 2023;
 SELECT "title", "format" FROM "longlist" WHERE "format" !='hardcover';
 SELECT "title", "translator" FROM "longlist" WHERE "translator" IS NULL;
 SELECT "title", "translator" FROM "longlist" WHERE "translator" IS NOT NULL;
+
+SELECT "title", "author" FROM "longlist" WHERE "year" >= 2019 AND "year" <= 2022;
+
+SELECT "title", "author" FROM "longlist" WHERE "year" BETWEEN 2019 AND 2022;
+
+SELECT "title", "rating" FROM "longlist" WHERE "rating" > 4.0;
+
+SELECT "title", "rating", "votes" FROM "longlist" WHERE "rating" > 4.0 AND "votes" > 10000;
+
+SELECT "title", "pages" FROM "longlist" WHERE "pages" < 300;
+
+
+
+
+
+
