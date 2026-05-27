3e350793d7a7fc52279420c7bb2bc845fa638d13
Mayuri Tapase <mayutapase@gmail.com>
2026-05-27 14:04:38 +0530

    Remove join query

--- lecture-0/demo.sql
+++ lecture-0/demo.sql
@@ -64,18 +64,9 @@ SELECT COUNT("translator") FROM "longlist";
 SELECT COUNT("publisher") FRON "longlist";
 SELECT COUNT("publisher") FROM "longlist";
 SELECT COUNT(DISTINCT "publisher") FROM "longlist";
-
 SELECT "id" FROM "publishers" WHERE "publisher" ='Maclehose Press';
-SELECT * FROM "sea_lions" JOIN "migrations" ON "migrations"."id" = "sea_lions"."id";
-SELECT * FROM "sea_lions" LEFT JOIN "migrations" ON "migrations"."id" = "sea_lions"."id";
 
-SELECT * FROM "sea_lions" RIGHT JOIN "migrations" ON "migrations"."id" = "sea_lions"."id";
-SELECT * FROM "sea_lions" FULL JOIN "migrations" ON "migrations"."id" = "sea_lions"."id";
-SELECT * FROM "sea_lions" NATURAL JOIN "migrations";
-SELECT * FROM sea_lions JOIN "migrations" JOIN "migrations" ON "migrations"."id" ="sea_lions"."id" where "migrations"."distance" >1500;
-SELECT * FROM "sea_lions" JOIN "migrations" ON "migrations"."id" = "sea_lions"."id" WHERE "migrations"."distance" > 1500
-;
-
+																																								
 
 
 
