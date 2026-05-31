a12e53fa7ced209c3571a9b7691f9232819d086f
Mayuri Tapase <mayutapase@gmail.com>
2026-05-31 12:30:15 +0530

    Add create table queries

---
+++ lecture-1/Create_table.sql
@@ -0,0 +1,26 @@
+select "author", "title" FROM "longlist" LIMIT 5;
+.schema books
+CREATE TABLE riders (
+"id",
+"name"
+);
+DROP TABLE "riders";
+DROP TABLE "stations";
+DROP TABLE "visits";
+CREATE TABLE riders (
+"id",
+"name"
+);
+CREATE TABLE visits (
+"rider_id",
+"station_id"
+);
+CREATE TABLE stations ( 
+"id",
+"name",
+"line"
+);
+DROP TABLE "riders";
+DROP TABLE "stations";
+DROP TABLE "visits";
+.schema
