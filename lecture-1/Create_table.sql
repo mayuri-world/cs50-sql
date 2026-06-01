211118c83cbaca71e0ce7d2a852d938f528d84cc
Mayuri Tapase <mayutapase@gmail.com>
2026-06-01 11:36:14 +0530

    Add more create querie

--- lecture-1/Create_table.sql
+++ lecture-1/Create_table.sql
@@ -24,3 +24,41 @@ DROP TABLE "riders";
 DROP TABLE "stations";
 DROP TABLE "visits";
 .schema
+
+CREATE TABLE riders (
+    "id" INTEGER,
+    "name" TEXT,
+    PRIMARY KEY("id")
+);
+
+CREATE TABLE stations (
+    "id" INTEGER,
+    "name" TEXT NOT NULL UNIQUE,
+    "line" TEXT NOT NULL,
+    PRIMARY KEY("id")
+);
+
+CREATE TABLE visits (
+    "rider_id" INTEGER,
+    "station_id" INTEGER,
+    FOREIGN KEY("rider_id") REFERENCES "riders"("id"),
+    FOREIGN KEY("station_id") REFERENCES "stations"("id")
+);
+
+DROP TABLE "riders";
+
+//...alter....
+
+ALTER TABLE "visits"
+RENAME TO "swipes";
+
+
+
+ALTER TABLE "swipes"
+ADD COLUMN "swipetype" TEXT;
+
+
+
+
+
+
