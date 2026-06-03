e3203702f44d5d7a429d42779f5cac8d5d404e83
Mayuri Tapase <mayutapase@gmail.com>
2026-06-03 11:02:36 +0530

    Add create table command

--- lecture-1/Create_table.sql
+++ lecture-1/Create_table.sql
@@ -52,11 +52,33 @@ DROP TABLE "riders";
 ALTER TABLE "visits"
 RENAME TO "swipes";
 
-
-
 ALTER TABLE "swipes"
 ADD COLUMN "swipetype" TEXT;
 
+
+CREATE TABLE "cards" (
+    "id" INTEGER,
+    PRIMARY KEY("id")
+);
+
+CREATE TABLE "stations" (
+    "id" INTEGER,
+    "name" TEXT NOT NULL UNIQUE,
+    "line" TEXT NOT NULL,
+    PRIMARY KEY("id")
+);
+
+CREATE TABLE "swipes" (
+    "id" INTEGER,
+    "card_id" INTEGER,
+    "station_id" INTEGER,
+    "type" TEXT NOT NULL CHECK("type" IN ('enter', 'exit', 'deposit')),
+    "datetime" NUMERIC NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "amount" NUMERIC NOT NULL CHECK("amount" != 0),
+    PRIMARY KEY("id"),
+    FOREIGN KEY("station_id") REFERENCES "stations"("id"),
+    FOREIGN KEY("card_id") REFERENCES "cards"("id")
+);
 
 
 
