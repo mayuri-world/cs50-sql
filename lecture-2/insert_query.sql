cdcc8b0c75e3d938d7a4f0aabc6a95a39b2c5949
Mayuri Tapase <mayutapase@gmail.com>
2026-06-08 14:48:53 +0530

    Add records into collection

--- lecture-2/insert_query.sql
+++ lecture-2/insert_query.sql
@@ -13,5 +13,16 @@ SELECT * FROM "collections";
 INSERT INTO "collections" ("title", "accession_number", "acquired")
 VALUES(NULL, NULL, '1900-01-10');
 
+INSERT INTO "collections" ("title", "accession_number", "acquired")
+VALUES ('Imaginative landscape', '56.496', NULL),
+('Peonies and butterfly', '06.1899', '1906-01-01');
+
+SELECT * FROM "collections";
+
+DELETE FROM "collections";
+
+SELECT * FROM "temp";
+
+
 
 
