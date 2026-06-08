c5903a05ef4426a697a8c862725d2af9e353f33c
Mayuri Tapase <mayutapase@gmail.com>
2026-06-08 14:27:05 +0530

    Add New inset file

---
+++ lecture-2/insert_query.sql
@@ -0,0 +1,17 @@
+SELECT * FROM "collections";
+
+INSERT INTO "collections" ("id", "title", "accession_number", "acquired")
+VALUES (1, 'Profusion of flowers', '56.257', '1956-04-12');
+
+SELECT * FROM "collections";
+
+INSERT INTO "collections" ("title", "accession_number", "acquired")
+VALUES ('Farmers working at dawn', '11.6152', '1911-08-03');
+
+SELECT * FROM "collections";
+
+INSERT INTO "collections" ("title", "accession_number", "acquired")
+VALUES(NULL, NULL, '1900-01-10');
+
+
+
