744a3965f5dccdbf83d781b130635bd43a6c0f7a
Mayuri Tapase <mayutapase@gmail.com>
2026-06-09 18:44:20 +0530

    Add some delete query

--- lecture-2/Delete_query.sql
+++ lecture-2/Delete_query.sql
@@ -1,3 +1,26 @@
 DELETE FROM "collections";
 
+DELETE FROM "collections"
+WHERE "title" = 'Spring outing';
+
+DELETE FROM "collections"
+WHERE "acquired" IS NULL;
+
+SELECT * FROM "collections";
+
+DELETE FROM "collections"
+WHERE "id" IN (1, 2);
+
+SELECT * FROM "collections";
+
+DELETE FROM "collections"
+WHERE "acquired" < '1909-01-01';
+
+SELECT * FROM "collections";
+
+
+
+
+
+
 
