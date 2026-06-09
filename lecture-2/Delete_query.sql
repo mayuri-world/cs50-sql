301abac176b3d5a6556c296d6a1f3f6e50a8e549
Mayuri Tapase <mayutapase@gmail.com>
2026-06-09 19:07:42 +0530

    Add more delete query

--- lecture-2/Delete_query.sql
+++ lecture-2/Delete_query.sql
@@ -18,6 +18,58 @@ WHERE "acquired" < '1909-01-01';
 
 SELECT * FROM "collections";
 
+DELETE FROM "artists"
+WHERE "name" = 'Unidentified artist';
+
+SELECT * FROM "artists";
+
+DELETE FROM "artists"
+WHERE "id" IN (SELECT "artist_id" FROM "collections");
+
+SELECT * FROM "artists";
+
+DELETE FROM "collections"
+WHERE "artist_id" IN (SELECT "id" FROM "artists");
+
+DELETE FROM "created"
+WHERE "artist_id" = (
+    SELECT "id"
+    FROM "artists"
+    WHERE "name" = 'Unidentified artist'
+);
+
+DELETE FROM "collections"
+WHERE "artist_id" = (
+    SELECT "id"
+    FROM "artists"
+    WHERE "name" = 'Unidentified artist'
+);
+
+DELETE FROM "artists"
+WHERE "name" = 'Unidentified artist';
+
+FOREIGN KEY("artist_id") REFERENCES "artists"("id") ON DELETE CASCADE
+FOREIGN KEY("collection_id") REFERENCES "collections"("id") ON DELETE CASCADE;
+
+DELETE FROM "artists"
+WHERE "name" = 'Unidentified artist';
+
+SELECT * FROM "artists";
+
+DELETE FROM "collections"
+WHERE "artist_id" IN (
+    SELECT "id"
+    FROM "artists"
+    WHERE "name" = 'Unidentified artist'
+);
+
+DELETE FROM "artists"
+WHERE "name" = 'Unidentified artist';
+
+SELECT * FROM "artists";
+
+SELECT * FROM "created";
+
 
 
 
