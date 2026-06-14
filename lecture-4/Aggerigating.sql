e5d554da9361bdc9ac76479568747919b482e379
Mayuri Tapase <mayutapase@gmail.com>
2026-06-14 19:49:07 +0530

    Add more practice query

--- lecture-4/Aggerigating.sql
+++ lecture-4/Aggerigating.sql
@@ -28,3 +28,38 @@ FROM "average_book_ratings"
 WHERE "rating" >= 4.5
 ORDER BY "rating" DESC;
 
+
+
+SELECT "book_id", ROUND(AVG("rating"), 2) AS "rating"
+FROM "ratings"
+GROUP BY "book_id";
+
+
+SELECT "book_id", "title", "year", ROUND(AVG("rating"), 2) AS "rating"
+FROM "ratings"
+JOIN "books" ON "ratings"."book_id" = "books"."id"
+GROUP BY "book_id";
+
+CREATE VIEW "average_book_ratings" AS
+SELECT "book_id" AS "id", "title", "year", ROUND(AVG("rating"), 2) AS "rating"
+FROM "ratings"
+JOIN "books" ON "ratings"."book_id" = "books"."id"
+GROUP BY "book_id";
+
+SELECT * FROM "average_book_ratings";
+
+SELECT "title", "year", "rating"
+FROM "average_book_ratings"
+WHERE "rating" >= 4.5
+ORDER BY "rating" DESC;
+
+SELECT "year", ROUND(AVG("rating"), 2) AS "rating"
+FROM "average_book_ratings"
+GROUP BY "year";
+
+CREATE TEMPORARY VIEW "average_ratings_by_year" AS
+SELECT "year", ROUND(AVG("rating"), 2) AS "rating" FROM "average_book_ratings"
+GROUP BY "year";
+
+
+
