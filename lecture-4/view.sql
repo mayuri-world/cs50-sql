46ecedc30ada660314d7dc06bff9268a1fa5e8ef
Mayuri Tapase <mayutapase@gmail.com>
2026-06-14 19:46:38 +0530

    Add more view practice query

--- lecture-4/view.sql
+++ lecture-4/view.sql
@@ -25,5 +25,53 @@ SELECT "title" FROM "longlist" WHERE "name" = 'Fernanda Melchor';
 
 SELECT "name", "title" FROM  "longlist" ORDER BY "title";
 
+SELECT "title" FROM "books"
+WHERE "id" IN (
+    SELECT "book_id" FROM "authored"
+    WHERE "author_id" = (
+        SELECT "id" FROM "authors"
+        WHERE "name" = 'Fernanda Melchor'
+    )
+);
+
+
+SELECT "name", "title" FROM "authors"
+JOIN "authored" ON "authors"."id" = "authored"."author_id"
+JOIN "books" ON "books"."id" = "authored"."book_id";
+
+
+
+CREATE VIEW "longlist" AS
+SELECT "name", "title" FROM "authors"
+JOIN "authored" ON "authors"."id" = "authored"."author_id"
+JOIN "books" ON "books"."id" = "authored"."book_id";
+
+
+SELECT * FROM "longlist";
+
+SELECT "title" FROM "longlist" WHERE "name" = 'Fernanda Melchor';
+
+SELECT "name", "title" FROM  "longlist" ORDER BY "title";
+
+
+
+SELECT "title" FROM "longlist" WHERE "name" = 'Fernanda Melchor';
+
+SELECT "name", "title" FROM  "longlist" ORDER BY "title";
+
+SELECT "name", "title"
+FROM  "longlist"
+ORDER BY "title";
+
+
+
+SELECT * FROM "longlist";
+
+
+
+
+
+
+
 
 
