abe951585fc51d6a6969df7193c2a261b700fb91
Mayuri Tapase <mayutapase@gmail.com>
2026-06-15 17:55:41 +0530

    Add Securing query

---
+++ lecture-4/Securing.sql
@@ -0,0 +1,20 @@
+
+
+CREATE VIEW "analysis" AS
+SELECT "id", "origin", "destination", 'Anonymous' AS "rider"
+FROM "rides";
+
+
+SELECT * FROM "analysis";
+
+CREATE VIEW "analysis" AS
+SELECT "id", "origin", "destination", 'Anonymous' AS "rider"
+FROM "rides";
+
+SELECT * FROM "analysis";
+
+CREATE VIEW "analysis" AS
+SELECT "id", "origin", "destination", 'Anonymous' AS "rider"
+FROM "rides";
+
+SELECT * FROM "analysis";
