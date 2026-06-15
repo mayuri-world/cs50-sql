a0d53158a394bebe2dbd88e03afb042cb43bf77b
Mayuri Tapase <mayutapase@gmail.com>
2026-06-15 17:52:25 +0530

    Add some partitioning query

---
+++ lecture-4/partitioning.sql
@@ -0,0 +1,34 @@
+
+
+CREATE VIEW "2022" AS
+SELECT "id", "title" FROM "books"
+WHERE "year" = 2022;
+
+SELECT * FROM "2022";
+CREATE VIEW "books_before_2000" AS
+SELECT "id", "title" FROM "books"
+WHERE "year" < 2000;
+
+SELECT * FROM "books_before_2000";
+CREATE VIEW "books_before_2000" AS
+SELECT "id", "title" FROM "books"
+WHERE "year" < 2000;
+
+SELECT * FROM "books_before_2000";
+
+CREATE VIEW "books_before_2000" AS
+SELECT "id", "title" FROM "books"
+WHERE "year" < 2000;
+
+SELECT * FROM "books_before_2000";
+
+CREATE VIEW "books_before_2000" AS
+SELECT "id", "title" FROM "books"
+WHERE "year" < 2000;
+
+
+
+SELECT * FROM "2022";
+
+SELECT * FROM "books_before_2000";
+
