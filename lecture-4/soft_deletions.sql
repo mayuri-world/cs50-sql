b0a2f1aa91b931de05b3c6dd8c3d0ddde9f02795
Mayuri Tapase <mayutapase@gmail.com>
2026-06-16 16:43:10 +0530

    Add some query for soft deletion

--- lecture-4/soft_deletions.sql
+++ lecture-4/soft_deletions.sql
@@ -14,3 +14,46 @@ SELECT * FROM "current_collections";
 
 SELECT * FROM "collections";
 
+
+CREATE TRIGGER "delete"
+INSTEAD OF DELETE ON "current_collections"
+FOR EACH ROW
+BEGIN
+    UPDATE "collections" SET "deleted" = 1
+    WHERE "id" = OLD."id";
+END;
+
+DELETE FROM "current_collections"
+WHERE "title" = 'Imaginative landscape';
+
+SELECT * FROM "current_collections";
+
+
+CREATE TRIGGER "insert_when_exists"
+INSTEAD OF INSERT ON "current_collections"
+FOR EACH ROW
+WHEN NEW."accession_number" IN (
+    SELECT "accession_number" FROM "collections"
+)
+BEGIN
+    UPDATE "collections"
+    SET "deleted" = 0
+    WHERE "accession_number" = NEW."accession_number";
+END;
+
+
+CREATE TRIGGER "insert_when_new"
+INSTEAD OF INSERT ON "current_collections"
+FOR EACH ROW
+WHEN NEW."accession_number" NOT IN (
+    SELECT "accession_number" FROM "collections"
+)
+BEGIN
+    INSERT INTO "collections" ("title", "accession_number", "acquired")
+    VALUES (NEW."title", NEW."accession_number", NEW."acquired");
+END;
+
+
+
+
+
