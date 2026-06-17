0e700b1254a1929f1a4fb5bd05aec273ad6ff0bb
Mayuri Tapase <mayutapase@gmail.com>
2026-06-17 20:02:45 +0530

    Add like queries

---
+++ lecture-0/Like.sql
@@ -0,0 +1,43 @@
+
+
+
+SELECT "title"
+FROM "longlist"
+WHERE "title" LIKE '%love%';
+
+SELECT "title"
+FROM "longlist"
+WHERE "title" LIKE 'The%';
+
+SELECT "title"
+FROM "longlist"
+WHERE "title" LIKE 'The %';
+
+SELECT "title"
+FROM "longlist"
+WHERE "title" LIKE 'P_re';
+
+SELECT "title"
+FROM "longlist"
+WHERE "title" LIKE 'P%re';
+
+SELECT "title"
+FROM "longlist"
+WHERE "title" LIKE 'P%re%';
+
+
+
+SELECT "title"
+FROM "longlist"
+WHERE "title" LIKE 'The%love%';
+
+
+SELECT "title"
+FROM "longlist"
+WHERE "title" LIKE 'T____';
+
+
+
+
+
+
