5c73c784fb35637f3f831f0e32ae98b17b3469ff
Mayuri Tapase <mayutapase@gmail.com>
2026-06-19 20:36:56 +0530

    Add some more query

--- lecture-2/updating_data.sql
+++ lecture-2/updating_data.sql
@@ -10,3 +10,37 @@ WHERE "collection_id" = (
     FROM "collections"
     WHERE "title" = 'Farmers working at dawn'
 );
+
+
+
+UPDATE "created"
+SET "artist_id" = (
+    SELECT "id"
+    FROM "artists"
+    WHERE "name" = 'Li Yin'
+)
+WHERE "collection_id" = (
+    SELECT "id"
+    FROM "collections"
+    WHERE "title" = 'Farmers working at dawn'
+);
+
+SELECT * FROM "created";
+
+UPDATE "collections"
+SET "artist_id" = (
+    SELECT "id"
+    FROM "artists"
+    WHERE "name" = 'Li Yin'
+)
+WHERE "title" = 'Farmers working at dawn';
+
+SELECT * FROM "collections";
+
+UPDATE "collections"
+SET "artist_id" = (
+    SELECT "id"
+    FROM "artists"
+    WHERE "name" = 'Li Yin'
+)
+WHERE "title" = 'Farmers working at dawn';
