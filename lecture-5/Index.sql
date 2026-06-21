97f8ec6749eedb7e27bda3061ed0a4aa8fa8fc9e
Mayuri Tapase <mayutapase@gmail.com>
2026-06-21 19:34:43 +0530

    Add query index across mulitiple table

--- lecture-5/Index.sql
+++ lecture-5/Index.sql
@@ -14,3 +14,44 @@ SELECT "title", "release_year" FROM "movies" WHERE "release_year" >= 2022 AND "r
 SELECT "title", "release_year" FROM "movies" WHERE "release_year" BETWEEN 2022 AND 2023;
 SELECT "title", "release_year" FROM "movies" WHERE "release_year" < 2022 OR "release_year" > 2023;
 SELECT "title", "release_year" FROM "movies" WHERE NOT ("release_year" >= 2022 AND "release_year" <= 2023);
+
+SELECT "title" FROM "movies"
+WHERE "id" IN (
+    SELECT "movie_id" FROM "stars"
+    WHERE "person_id" = (
+        SELECT "id" FROM "people"
+        WHERE "name" = 'Tom Hanks'
+    )
+);
+
+SELECT "title" FROM "movies"
+WHERE "id" IN (
+    SELECT "movie_id" FROM "stars"
+    WHERE "person_id" IN (
+        SELECT "id" FROM "people"
+        WHERE "name" IN ('Tom Hanks', 'Meryl Streep')
+    )
+);
+
+SELECT "title" FROM "movies"
+WHERE "id" IN (
+    SELECT "movie_id" FROM "stars"
+    WHERE "person_id" IN (
+        SELECT "id" FROM "people"
+        WHERE "name" IN ('Tom Hanks', 'Meryl Streep')
+    )
+) AND "release_year" >= 2022;
+
+SELECT "title" FROM "movies"
+WHERE "id" IN (
+    SELECT "movie_id" FROM "stars"
+    WHERE "person_id" IN (
+        SELECT "id" FROM "people"
+        WHERE "name" IN ('Tom Hanks', 'Meryl Streep')
+    )
+) AND "release_year" >= 2022
+
+ORDER BY "release_year" DESC;
+
+
+
