5d37422e0c47ea13ddd6250298d440b73efb7f0f
Mayuri Tapase <mayutapase@gmail.com>
2026-06-22 19:15:45 +0530

    Add more where practice query

--- lecture-0/where.sql
+++ lecture-0/where.sql
@@ -34,3 +34,18 @@ SELECT "title", "author"
 FROM "longlist"
 WHERE NOT ("year" >= 2022 AND "year" <= 2023);
 
+SELECT "title", "format"
+FROM "longlist"
+WHERE "format" != 'hardcover';
+
+SELECT "title", "format"
+FROM "longlist"
+WHERE "format" <> 'hardcover';
+
+SELECT "title", "format"
+FROM "longlist"
+WHERE NOT "format" = 'hardcover';
+
+SELECT "title", "author"
+FROM "longlist"
+WHERE "year" = 2022 OR "year" = 2023;
