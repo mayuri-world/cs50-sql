c1854722c409352746b11ca0ad2d10d8749ce397
Mayuri Tapase <mayutapase@gmail.com>
2026-06-23 10:38:53 +0530

    Add Limit sql file

---
+++ lecture-0/Limit.sql
@@ -0,0 +1,8 @@
+
+SELECT "title" FROM "longlist" LIMIT 10;
+select * from "longlist" limit 3;
+SELECT "title", "author" FROM "longlist" LIMIT 5;
+SELECT "title", "author" FROM "longlist" LIMIT 5 OFFSET 5;
+SELECT "title", "author" FROM "longlist" LIMIT 5 OFFSET 10;
+SELECT "author" from "longlist" LIMIT 10;
+
