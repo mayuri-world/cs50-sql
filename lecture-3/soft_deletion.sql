831ca53b69da3b5569f986dcfb692c24b55e80a6
Mayuri Tapase <mayutapase@gmail.com>
2026-06-24 21:29:37 +0530

    Add new query

--- lecture-3/soft_deletion.sql
+++ lecture-3/soft_deletion.sql
@@ -10,8 +10,6 @@ WHERE "title" = 'Farmers working at dawn';
 SELECT * FROM "collections"
 WHERE "deleted" != 1;
 
-
-
 ALTER TABLE "collections"
 ADD COLUMN "deleted" INTEGER DEFAULT 0;
 
@@ -42,5 +40,19 @@ BEFORE DELETE ON "collections"
 BEGIN
     INSERT INTO "transactions" ("title", "action")
     VALUES (OLD."title", 'sold');
+END;
+
+ALTER TABLE "collections"
+ADD COLUMN "deleted" INTEGER DEFAULT 0;
+
+CREATE TRIGGER "soft_delete"
+BEFORE DELETE ON "collections"
+BEGIN
+    UPDATE "collections"
+SET "deleted" = 1
+    WHERE "id" = OLD."id";
+    INSERT INTO "transactions" ("title", "action")
+    VALUES (OLD."title", 'sold');
+    SELECT RAISE(IGNORE);
 END;
 
