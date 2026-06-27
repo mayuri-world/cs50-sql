b25b377185701961d5edb8e6efd29d191e0595f6
Mayuri Tapase <mayutapase@gmail.com>
2026-06-27 17:56:49 +0530

    Add some more temporary view query

--- lecture-4/view.sql
+++ lecture-4/view.sql
@@ -83,7 +83,23 @@ SELECT "year", ROUND(AVG("rating"), 2) AS "rating" FROM "average_book_ratings"
 GROUP BY "year";
 
 
+CREATE TEMPORARY VIEW "average_ratings_by_year" AS
+WITH "average_book_ratings" AS (
+    SELECT "book_id", "title", "year", ROUND(AVG("rating"), 2) AS "rating" FROM "ratings"
+    JOIN "books" ON "ratings"."book_id" = "books"."id"
+    GROUP BY "book_id"
+)
+SELECT "year", ROUND(AVG("rating"), 2) AS "rating" FROM "average_book_ratings"
+GROUP BY "year";
 
+CreATE TEMPORARY VIEW "average_ratings_by_year" AS
+WITH "average_book_ratings" AS (
+    SELECT "book_id", "title", "year", ROUND(AVG("rating"), 2) AS "rating" FROM "ratings"
+    JOIN "books" ON "ratings"."book_id" = "books"."id"
+    GROUP BY "book_id"
+)
+SELECT "year", ROUND(AVG("rating"), 2) AS "rating" FROM "average_book_ratings"
+GROUP BY "year";
 
 
 
