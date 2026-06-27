2a41e5a7d21aafbf78ef65ed3caa991f24636050
Mayuri Tapase <mayutapase@gmail.com>
2026-06-27 17:53:32 +0530

    Add temprorary view

--- lecture-4/view.sql
+++ lecture-4/view.sql
@@ -1,18 +1,11 @@
 
 
-SELECT "title" FROM "books"
-WHERE "id" IN (
-SELECT "book_id" FROM "authored"
-WHERE "author_id" = (
-SELECT "id" FROM "authors"
-WHERE "name" = 'Fernanda Melchor'
-    )
-);
 
 SELECT "name", "title" FROM "authors"
 JOIN "authored" ON "authors"."id" = "authored"."author_id"
 JOIN "books" ON "books"."id" = "authored"."book_id";
 
+/*A view is a virtual table defined by a query.*/
 
 CREATE VIEW "longlist" AS
 SELECT "name", "title" FROM "authors"
@@ -38,7 +31,6 @@ WHERE "id" IN (
 SELECT "name", "title" FROM "authors"
 JOIN "authored" ON "authors"."id" = "authored"."author_id"
 JOIN "books" ON "books"."id" = "authored"."book_id";
-
 
 
 CREATE VIEW "longlist" AS
@@ -75,8 +67,20 @@ WHERE "id" IN (
 
 SELECT * FROM "longlist";
 
+CREATE VIEW "longlist" AS
+SELECT "name", "title" FROM "authors"
+JOIN "authored" ON "authors"."id" = "authored"."author_id"
+JOIN "books" ON "books"."id" = "authored"."book_id";
 
+/* We can store the results in a temporary view.*/
 
+CREATE TEMPORARY VIEW "average_ratings_by_year" AS
+SELECT "year", ROUND(AVG("rating"), 2) AS "rating" FROM "average_book_ratings"
+GROUP BY "year";
+
+CREATE TEMPORARY VIEW "average_ratings_by_year" AS
+SELECT "year", ROUND(AVG("rating"), 2) AS "rating" FROM "average_book_ratings"
+GROUP BY "year";
 
 
 
