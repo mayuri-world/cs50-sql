1c51ef45922071f23c7a7309f2e2f35933162cba
Mayuri Tapase <mayutapase@gmail.com>
2026-06-29 20:23:42 +0530

    Add comments

--- lecture-5/partial_index.sql
+++ lecture-5/partial_index.sql
@@ -1,4 +1,6 @@
 
+/* this is an index that includes only a subset of rows from a table, allowing us to save some space that a full index would occupy.
+This is especially useful when we know that users query only a subset of rows from the table. In the case of IMDb, it may be that the users are more likely to query a movie that was just released as opposed to a movie that is 15 years old. Let’s try to create a partial index that stores the titles of movies released in 2023.*/
 
 
 CREATE INDEX "recents" ON "movies" ("titles")
