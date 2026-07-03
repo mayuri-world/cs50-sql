7f3cc28b5be50a7d2461073619ac462aab1cce55
Mayuri Tapase <mayutapase@gmail.com>
2026-07-03 18:19:04 +0530

    Add transaction query

--- lecture-5/Transactions.sql
+++ lecture-5/Transactions.sql
@@ -7,3 +7,19 @@ Transactions have some properties, which can be remembered using the acronym ACI
     isolation: if multiple users access a database, their transactions cannot interfere with each other,
     durability: in case of any failure within the database, all data changed by transactions will remain.
 */
+
+
+
+SELECT * FROM "accounts";
+
+
+BEGIN TRANSACTION;
+UPDATE "accounts" SET "balance" = "balance" + 10 WHERE "id" = 2;
+UPDATE "accounts" SET "balance" = "balance" - 10 WHERE "id" = 1;
+COMMIT;
+
+ BEGIN TRANSACTION;
+UPDATE "accounts" SET "balance" = "balance" + 10 WHERE "id" = 2;
+UPDATE "accounts" SET "balance" = "balance" - 10 WHERE "id" = 1;
+ROLLBACK;
+
