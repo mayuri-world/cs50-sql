e1920296718157e263944369e2c33e84e6fcc791
Mayuri Tapase <mayutapase@gmail.com>
2026-07-03 18:13:59 +0530

    Add some comments into file

---
+++ lecture-5/Transactions.sql
@@ -0,0 +1,9 @@
+
+/*
+Transactions have some properties, which can be remembered using the acronym ACID:
+
+    atomicity: can’t be broken down into smaller pieces,
+    consistency: should not violate a database constraint,
+    isolation: if multiple users access a database, their transactions cannot interfere with each other,
+    durability: in case of any failure within the database, all data changed by transactions will remain.
+*/
