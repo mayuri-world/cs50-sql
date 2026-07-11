ed2d77fcc93a286fa64a9805a19adbd1bd8c3723
Mayuri Tapase <mayutapase@gmail.com>
2026-07-11 21:02:12 +0530

    Create some index

--- lecture-5/Index.sql
+++ lecture-5/Index.sql
@@ -15,44 +15,8 @@ SELECT "title", "release_year" FROM "movies" WHERE "release_year" BETWEEN 2022 A
 SELECT "title", "release_year" FROM "movies" WHERE "release_year" < 2022 OR "release_year" > 2023;
 SELECT "title", "release_year" FROM "movies" WHERE NOT ("release_year" >= 2022 AND "release_year" <= 2023);
 
-SELECT "title" FROM "movies"
-WHERE "id" IN (
-    SELECT "movie_id" FROM "stars"
-    WHERE "person_id" = (
-        SELECT "id" FROM "people"
-        WHERE "name" = 'Tom Hanks'
-    )
-);
 
-SELECT "title" FROM "movies"
-WHERE "id" IN (
-    SELECT "movie_id" FROM "stars"
-    WHERE "person_id" IN (
-        SELECT "id" FROM "people"
-        WHERE "name" IN ('Tom Hanks', 'Meryl Streep')
-    )
-);
-
-SELECT "title" FROM "movies"
-WHERE "id" IN (
-    SELECT "movie_id" FROM "stars"
-    WHERE "person_id" IN (
-        SELECT "id" FROM "people"
-        WHERE "name" IN ('Tom Hanks', 'Meryl Streep')
-    )
-) AND "release_year" >= 2022;
-
-SELECT "title" FROM "movies"
-WHERE "id" IN (
-    SELECT "movie_id" FROM "stars"
-    WHERE "person_id" IN (
-        SELECT "id" FROM "people"
-        WHERE "name" IN ('Tom Hanks', 'Meryl Streep')
-    )
-) AND "release_year" >= 2022
-
-ORDER BY "release_year" DESC;
-
+/* Indexes: data structures that allow us to quickly look up data in a table. */
 
 
 CREATE INDEX "title_index" ON "movies" ("title");
@@ -64,3 +28,14 @@ SELECT "title" FROM "movies" WHERE "title" = 'The Fabelmans';
 SELECT "title" FROM "movies" WHERE "title" = 'The Fabelmans'
 AND "release_year" = 2022;
 
+
+
+CREATE INDEX "title_index" ON "movies" ("title");
+
+CREATE INDEX "title_year_index" ON "movies" ("title", "release_year");
+
+CREATE INDEX "release_year_index" ON "movies" ("release_year");
+
+CREATE INDEX "release_year_title_index" ON "movies" ("release_year", "title");
+
+CREATE INDEX "release_year_title_index" ON "movies" ("release_year", "title");
