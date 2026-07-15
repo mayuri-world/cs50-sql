60a1dc89803e08fa412f75e62ef4084763ec2e88
Mayuri Tapase <mayutapase@gmail.com>
2026-07-15 14:57:05 +0530

    Add some more group by query

--- lecture-0/group_by.sql
+++ lecture-0/group_by.sql
@@ -23,4 +23,38 @@ HAVING "average rating" > 4.0
 ORDER BY "average rating" DESC
 LIMIT 10;
 
+SELECT "book_id", ROUND(AVG("rating"), 2) AS "average rating"
+FROM "ratings"
+GROUP BY "book_id"
+HAVING "average rating" > 4.0
+ORDER BY "average rating" DESC
+LIMIT 10 OFFSET 10;
+
+SELECT "book_id", COUNT("rating")
+FROM "ratings"
+GROUP BY "book_id";
+
+SELECT "book_id", COUNT("rating") AS "number of ratings"
+FROM "ratings"
+GROUP BY "book_id"
+ORDER BY "number of ratings" DESC
+LIMIT 10;
+
+SELECT "book_id", COUNT("rating") AS "number of ratings"
+FROM "ratings"
+GROUP BY "book_id"
+HAVING "number of ratings" > 1000
+ORDER BY "number of ratings" DESC
+LIMIT 10;
+
+SELECT "book_id", COUNT("rating") AS "number of ratings"
+FROM "ratings"
+GROUP BY "book_id"
+HAVING "number of ratings" > 1000
+ORDER BY "number of ratings" DESC
+LIMIT 10 OFFSET 10;
+
+
+
+
 
