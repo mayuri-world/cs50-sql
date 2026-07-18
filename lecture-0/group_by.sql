faef41bdf99625e145a5b2ff27f8349536cf60f2
Mayuri Tapase <mayutapase@gmail.com>
2026-07-18 09:45:14 +0530

    Add some more practice group by queries

--- lecture-0/group_by.sql
+++ lecture-0/group_by.sql
@@ -56,5 +56,19 @@ LIMIT 10 OFFSET 10;
 
 
 
+SELECT "book_id", ROUND(AVG("rating"), 2) AS "average rating"
+FROM "ratings"
+GROUP BY "book_id"
+HAVING "average rating" > 4.0
+ORDER BY "average rating" DESC;
+
+SELECT "book_id", ROUND(AVG("rating"), 2) AS "average rating"
+FROM "ratings"
+GROUP BY "book_id"
+HAVING "average rating" > 4.0
+ORDER BY "average rating" DESC
+LIMIT 10;
+
+
 
 
