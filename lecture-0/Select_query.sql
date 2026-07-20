ebe4bdab9470e4c80cf3a18aed5c0767927f9bbf
Mayuri Tapase <mayutapase@gmail.com>
2026-07-20 19:17:49 +0530

    Add select query

---
+++ lecture-0/Select_query.sql
@@ -0,0 +1,39 @@
+
+
+SELECT "author", "title"
+FROM "longlist"
+LIMIT 10;
+
+SELECT "author", "title"
+FROM "longlist"
+WHERE "year" = 2022
+LIMIT 10;
+
+SELECT "author", "title"
+FROM "longlist"
+WHERE "year" = 2021
+LIMIT 10;
+
+SELECT "author", "title"
+FROM "longlist"
+WHERE "year" = 2020
+LIMIT 10;
+
+SELECT "author", "title"
+FROM "longlist"
+WHERE "year" = 2019
+LIMIT 10;
+
+SELECT * FROM "longlist"
+WHERE "year" = 2018
+LIMIT 10;
+
+SELECT "author", "title"
+FROM "longlist"
+WHERE "year" = 2017
+LIMIT 10;
+
+SELECT "author", "title"
+FROM "longlist"
+WHERE "year" = 2016
+LIMIT 10;
