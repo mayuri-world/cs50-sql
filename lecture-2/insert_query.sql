1f3916a8cbe9dd77386821d141fbd9b34ee7e049
Mayuri Tapase <mayutapase@gmail.com>
2026-07-20 19:21:57 +0530

    Inserted some new records

--- lecture-2/insert_query.sql
+++ lecture-2/insert_query.sql
@@ -37,6 +37,18 @@ VALUES ('Imaginative landscape', '56.496', NULL),
 ('Peonies and butterfly', '06.1899', '1906-01-01');
 
 
+INSERT INTO "collections" ("id", "title", "accession_number", "acquired")
+VALUES (1, 'Profusion of flowers', '56.257', '1956-04-13');
+
+INSERT INTO "collections" ("title", "accession_number", "acquired")
+VALUES ('Farmers working at dawn', '11.6152', '1911-08-03');
+
+INSERT INTO "collections" ("title", "accession_number", "acquired")
+VALUES(NULL, NULL, '1900-01-10');
+
+
+
+
 
 
 
