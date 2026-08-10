840d9318c05b002ea564e527889d4555dfcde53d
Mayuri Tapase <mayutapase@gmail.com>
2026-08-10 19:01:17 +0530

    Remove some query

--- lecture-5/Transactions.sql
+++ lecture-5/Transactions.sql
@@ -17,29 +17,9 @@ UPDATE "accounts" SET "balance" = "balance" + 10 WHERE "id" = 2;
 UPDATE "accounts" SET "balance" = "balance" - 10 WHERE "id" = 1;
 COMMIT;
 
- BEGIN TRANSACTION;
-UPDATE "accounts" SET "balance" = "balance" + 10 WHERE "id" = 2;
-UPDATE "accounts" SET "balance" = "balance" - 10 WHERE "id" = 1;
-ROLLBACK;
-
-BEGIN TRANSACTION;
-UPDATE "accounts" SET "balance" = "balance" + 10 WHERE "id" = 2;
-UPDATE "accounts" SET "balance" = "balance" - 10 WHERE "id" = 1;
-COMMIT;
-
-BEGIN TRANSACTION;
-UPDATE "accounts" SET "balance" = "balance" + 10 WHERE "id" = 2;
-UPDATE "accounts" SET "balance" = "balance" - 10 WHERE "id" = 1;
-ROLLBACK;
-
-
 
 BEGIN TRANSACTION;
 UPDATE "accounts" SET "balance" = "balance" + 10 WHERE "id" = 2;
 UPDATE "accounts" SET "balance" = "balance" - 10 WHERE "id" = 1; -- Invokes constraint error
 ROLLBACK;
 
-BEGIN TRANSACTION;
-UPDATE "accounts" SET "balance" = "balance" + 10 WHERE "id" = 2;
-UPDATE "accounts" SET "balance" = "balance" - 10 WHERE "id" = 1; -- Invokes constraint error
-COMMIT;
