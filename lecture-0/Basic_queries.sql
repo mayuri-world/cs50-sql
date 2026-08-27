7b0e1ed04d1603c1ab2059e501f5a83607f662cd
Mayuri Tapase <mayutapase@gmail.com>
2026-08-27 18:07:34 +0530

    Updated some queries

--- lecture-0/Basic_queries.sql
+++ lecture-0/Basic_queries.sql
@@ -1,4 +1,3 @@
-select * from "longlist" limit 3;
 
 select "author" from "longlist";
 
@@ -6,53 +5,16 @@ select "title", "author" from "longlist" where "year"=2022;
 
 select "title", "author" from "longlist" where "year"=2021;
 
-SELECT "title", "format"
-FROM "longlist"
-WHERE "format" != 'hardcover';
-
-SELECT "title", "format"
-FROM "longlist"
-WHERE "format" <> 'hardcover';
-
-SELECT "title", "format"
-FROM "longlist"
-WHERE NOT "format" = 'hardcover';
-
-SELECT "title", "author"
-FROM "longlist"
-WHERE "year" = 2022 OR "year" = 2023;
-
 select * FROM "longlist";
 select "title" FROM "longlist";
 select "title", "author" FROM longlist;
-SELECT "title" from "longlist" LIMIT 10;
 SELECT "title" , "author" FROM "longlist" WHERE "fromat" !='hardcover';
 SELECT "title", "author" FROM "longlist" WHERE "year"= 2023;
 SELECT "title", "format" FROM "longlist" WHERE "format" !='hardcover';
 SELECT "title", "translator" FROM "longlist" WHERE "translator" IS NULL;
 SELECT "title", "translator" FROM "longlist" WHERE "translator" IS NOT NULL;
 
-SELECT "title", "author" FROM "longlist" WHERE "year" >= 2019 AND "year" <= 2022;
 
-SELECT "title", "author" FROM "longlist" WHERE "year" BETWEEN 2019 AND 2022;
-
-SELECT "title", "rating" FROM "longlist" WHERE "rating" > 4.0;
-
-SELECT "title", "rating", "votes" FROM "longlist" WHERE "rating" > 4.0 AND "votes" > 10000;
-
-SELECT "title", "pages" FROM "longlist" WHERE "pages" < 300;
-
-
-SELECT "title", "rating"
-FROM "longlist"
-ORDER BY "rating" LIMIT 10;
-
-SELECT "title", "rating"
-FROM "longlist"
-ORDER BY "rating" DESC LIMIT 10;
-
-SELECT "title", "rating", "votes" FROM "longlist" ORDER BY "rating" DESC, "votes" DESC LIMIT 10;
-SELECT "title" FROM "longlist" ORDER BY "title";
 
 SELECT "id" FROM "publishers" WHERE "publisher" ='Maclehose Press';
 
